afff224f2a314cdb819ab32ea27cdcc4bf7beecc
Ajay Gokhale <ajay.g@callme.co.in>
2020-12-21 16:34:05 +0530

    changes made in project adn navigation bar added.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import Counters from "./components/counters";
 import "bootstrap/dist/css/bootstrap.css";
+import App from "./App";
 
-ReactDOM.render(<Counters />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
